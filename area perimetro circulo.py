6356942e8662b9ff62d0fb84222f16d18b393512
Rolando Parra Julio <rolandoparra@colegioguanenta.edu.co>
2026-01-28 08:58:35 -0500

    Add files via upload

---
+++ area perimetro circulo.py
@@ -0,0 +1,25 @@
+#inicio XD
+r = input("digite el radio del círculo: ")
+π = 3.14159
+a = 0
+p = 0
+
+#proceso XDXDXDDD
+print()
+print("procesando...")
+print()
+
+r = int(r)
+
+a = π * (r * r)
+p = 2 * π * r
+
+#lo que sale :P
+print("el área del círculo es: ")
+print(a)
+print("el perímetro del círculo es:")
+print(p)
+
+print()
+print("(presione Enter para salir)")
+input()
